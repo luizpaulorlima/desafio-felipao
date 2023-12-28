84b54abdb53ca0033c73c75b50561a7d52e7db94
luizpaulorlima <luizpaulo.rlima@gmail.com>
2023-12-28 16:45:18 -0300

    Desafio Classificador de Nível de Herói

---
+++ src/index.js
@@ -0,0 +1,20 @@
+let nomeHeroi = "Luiz"
+let experienciaHeroi = 1001
+
+if (experienciaHeroi <= 1000){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível " + "Ferro")
+} else if(experienciaHeroi <= 2000){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível " + "Bronze")
+} else if(experienciaHeroi <= 5000){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível " + "Prata")
+} else if(experienciaHeroi <= 7000){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível " + "Ouro")
+} else if(experienciaHeroi <= 8000){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível " + "Platina")
+} else if(experienciaHeroi <= 9000){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível " + "Ascedente")
+} else if(experienciaHeroi <= 10000){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível " + "Imortal")
+} else if (experienciaHeroi >= 10001){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível " + "Radiante")
+} 
